fec68421019d8f6b9d609734aa93dabdbe8541cd
AlexandrTolstov <alextav_85@mail.ru>
2019-06-25 21:32:14 +0600

    Add project files.

---
+++ SimpleDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+    public class SimpleMath
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Simple Delegate Example ******\n");
+            BinaryOp b = new BinaryOp(SimpleMath.Add);
+            Console.WriteLine("10 + 10 is {0}", b(10, 10));
+            Console.ReadLine();
+        }
+    }
+}
