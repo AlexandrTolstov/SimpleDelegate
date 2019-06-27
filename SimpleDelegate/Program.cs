01be87b443f98b0c1024be8c249a072d7578dcb9
Толстов Александр <alextav_85@mail.ru>
2019-06-27 14:28:55 +0600

    1

--- SimpleDelegate/Program.cs
+++ SimpleDelegate/Program.cs
@@ -9,17 +9,30 @@ namespace SimpleDelegate
     public delegate int BinaryOp(int x, int y);
     public class SimpleMath
     {
-        public static int Add(int x, int y) => x + y;
-        public static int Subtract(int x, int y) => x - y;
+        public int Add(int x, int y) => x + y;
+        public int Subtract(int x, int y) => x - y;
+        public int SquareNumber(int a) => a * a;
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("****** Simple Delegate Example ******\n");
-            BinaryOp b = new BinaryOp(SimpleMath.Add);
+            SimpleMath m = new SimpleMath();
+            BinaryOp b = new BinaryOp(m.Add);
             Console.WriteLine("10 + 10 is {0}", b(10, 10));
+
+            DisplayDelegateInfo(b);
             Console.ReadLine();
+        }
+
+        static void DisplayDelegateInfo(Delegate delObj)
+        {
+            foreach (Delegate d in delObj.GetInvocationList())
+            {
+                Console.WriteLine("Method Name: {0}", d.Method);
+                Console.WriteLine("Type Name: {0}", d.Target);
+            }
         }
     }
 }
